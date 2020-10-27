e871c7eef96e9bedd34304fa9736ad19e837c57c
Chrosnik-Jakub <73523188+Chrosnik-Jakub@users.noreply.github.com>
2020-10-27 10:26:10 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="con">
 
-$conn= new mysqli("127.0.0.1","root","","nauka");
+    <div class="a">
+         <?php
 
-    $result=$conn->query('SELECT * FROM pracownicy');
+            $conn= new mysqli("127.0.0.1","root","","nauka");
 
-        echo("<table border=1>");
-        echo("<th>id</th>");
-        echo("<th>imie</th>");
-        echo("<th>dzial</th>");
-        echo("<th>zarobki</th>");
+                  $result=$conn->query('SELECT * FROM pracownicy');
 
-    while($row=$result->fetch_assoc()){
+                    echo("<table border=1>");
+                    echo("<th>id</th>");
+                    echo("<th>imie</th>");
+                    echo("<th>dzial</th>");
+                    echo("<th>zarobki</th>");
 
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
-    echo("</tr>");
-}
-    echo("</table>");
-?>
+
+            while($row=$result->fetch_assoc()){
+
+                    echo("<tr>");
+                    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>");
+                    echo("</tr>");
+                    }
+                    echo("</table>");
+        ?>
+</div>
+</div>
+</body>
+</html>
